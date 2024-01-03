c4c5c95af237d900cbe7d25768218e6e34a77d85
Miguel Bessa <miguelferreira871@gmail.com>
2024-01-03 23:07:40 +0000

    melhorar css do feed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,35 +11,43 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <!-- Seção dos botões -->
                     <div id="category-buttons" class="mb-4">
-                        <button onclick="filterByCategory('sport')" class="bg-blue-500 text-black py-2 px-4 rounded">Desporto</button>
-                        <button onclick="filterByCategory('culture')" class="bg-blue-500 text-black py-2 px-4 rounded">Cultura</button>
-                        <button onclick="filterByCategory('gastronomy')" class="bg-blue-500 text-black py-2 px-4 rounded">Gastronomia</button>
-                        <button onclick="filterByCategory('nature')" class="bg-blue-500 text-black py-2 px-4 rounded">Natureza</button>
+                        <button onclick="filterByCategory('sport')" class="bg-blue-500 text-white py-2 px-4 rounded">Desporto</button>
+                        <button onclick="filterByCategory('culture')" class="bg-blue-500 text-white py-2 px-4 rounded">Cultura</button>
+                        <button onclick="filterByCategory('gastronomy')" class="bg-blue-500 text-white py-2 px-4 rounded">Gastronomia</button>
+                        <button onclick="filterByCategory('nature')" class="bg-blue-500 text-white py-2 px-4 rounded">Natureza</button>
                         <!--<button onclick="window.location='{{url('/dashboard')}} '" class="bg-blue-500 text-black py-2 px-4 rounded">Feed</button> -->
                     </div>
 
                     <!-- Seção de exibição das atividades -->
                     <div id="activities-section" class="flex flex-wrap -mx-2">
                         @foreach ($experiences as $index => $experience)
-                            <div class="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 px-2 mb-4 experience {{ $experience['category'] }}">
-                                <div class="bg-white  rounded border pl-4 py-4 pr-20">
-                                    <img src="{{ $experience['image'] }}" alt="{{ $experience['title'] }}" class="w-full mb-2 rounded" />
-                                    <h3 class="text-lg font-semibold">{{ $experience['title'] }}</h3>
-                                    <p class="text-gray-700">Descrição: {{ $experience['description'] }}.</p>
-                                    <p class="text-gray-800 font-bold">Preço: {{ $experience['price'] }}€</p>
-                                    <p class="text-gray-800 font-bold">Morada: {{ $experience['address'] }}.</p>
-                                    <p class="text-gray-800 font-bold mb-6">Categoria: {{ $experience['category'] }}.</p>
-                                    
-                                    <form method="post" action="{{ route('like.store') }}" class="inline-block">
-                                        @csrf
-                                        <input type="hidden" name="experience_id" value="{{ $experience['id'] }}">
-                                        <button type="submit" class="text-blue-500 no-underline border border-blue-500 px-2 py-0 rounded">Like</button>
-                                    </form>
+                            <div class="relative w-full sm:w-1/2 md:w-1/4 lg:w-1/4 px-2 experience {{ $experience['category'] }}">
+                                <div class="w-full h-full m-0 bg-white rounded border border-black pl-4 pt-4 pb-20 pr-20">
 
-                                    <span class="text-gray-800 font-bold ml-2">Likes: {{ $experience->likes->count() }}</span>
+                                    <div>
+                                        <img src="{{ $experience['image'] }}" alt="{{ $experience['title'] }}" class="w-full mb-2 rounded" />
+                                        <h3 class="text-lg font-semibold">{{ $experience['title'] }}</h3>
+                                        <p class="text-gray-700">Descrição: {{ $experience['description'] }}.</p>
+                                        <p class="text-gray-800 font-bold">Preço: {{ $experience['price'] }}€</p>
+                                        <p class="text-gray-800 font-bold">Morada: {{ $experience['address'] }}.</p>
+                                        <p class="text-gray-800 font-bold mb-6">Categoria: {{ $experience['category'] }}.</p>
+                                    </div>
+
+                                    <div class="absolute bottom-2">
+                                        <form method="post" action="{{ route('like.store') }}" class="mb-2">
+                                            @csrf
+                                            <input type="hidden" name="experience_id" value="{{ $experience['id'] }}">
+                                            <button type="submit" class="text-blue-500 no-underline border-2 border-blue-500 px-2 py-1 rounded">Like</button>
+                                            <span class="text-gray-800 font-bold ml-2">Likes: {{ $experience->likes->count() }}</span>
+                                        </form>
+                                        
+                                        <div class="flex space-x-2">
+                                            <a href="{{ route('comment.create', ['experience_id' => $experience['id']]) }}" class="text-blue-500 no-underline border-2 border-blue-500 px-2 py-1 rounded">Comentar</a>
+                                            <a href="{{ route('experience.comments', ['experience_id' => $experience['id']]) }}" class="text-blue-500 no-underline border-2 border-blue-500 px-2 py-1 rounded">Comentário</a>
+                                        </div>
+                                    </div>
                             
-                                    <a href="{{ route('comment.create', ['experience_id' => $experience['id']]) }}" class="text-blue-500 no-underline border border-blue-500 px-2 py-0 rounded">Comentar</a>
-                                    <a href="{{ route('experience.comments', ['experience_id' => $experience['id']]) }}" class="text-blue-500 no-underline border border-blue-500 px-2 py-0 rounded">Comentários</a>
+                                    
                                 </div>
                             </div>
 
